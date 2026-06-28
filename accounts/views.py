ed109e610ddd0f948a47b937cf5d986f46f02c88
CodeGhost <bellinuolaji@gmail.com>
2026-06-28 14:28:23 +0100

    update views.py for account

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
 from .serializers import RegisterSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework.parsers import MultiPartParser, FormParser 
 
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+    parser_classes = [MultiPartParser, FormParser]
